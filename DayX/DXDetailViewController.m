5e82e13476dce636351089ae8ee48198120a6fea
James Westmoreland <jr@jrw.org>
2014-09-18 13:35:31 -0600

    Seems to be something wrong with responders.

--- DayX/DXDetailViewController.m
+++ DayX/DXDetailViewController.m
@@ -23,18 +23,29 @@
     
     self.title = @"DayX";
     
-    self.titleField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+    self.titleField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width - 60, 44)];
+    self.titleField.text = @"Enter Title Here";
     self.titleField.clearButtonMode = UITextFieldViewModeAlways;
     self.titleField.delegate = self;
     [self.view addSubview:self.titleField];
     
     self.textNote = [[UITextView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height - 64)];
+    self.textNote.text = @"Place notes and other related text in this area.";
     self.textNote.delegate = self;
     [self.view addSubview:self.textNote];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
+    return YES;
+}
+
+// called when clear button pressed. return NO to ignore
+- (BOOL)textFieldShouldClear:(UITextField *)textField
+{
+    self.titleField = @"";
+    self.textNote = @"";
+    
     return YES;
 }
 
