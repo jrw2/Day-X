1d97813bd1bcb13129ef1212d7340b0787f16053
James Westmoreland <jr@jrw.org>
2014-09-20 12:28:47 -0600

    Removed temporary text from fields.

--- DayX/DXDetailViewController.m
+++ DayX/DXDetailViewController.m
@@ -26,13 +26,13 @@
     self.title = @"DayX";
     
     self.titleField = [[UITextField alloc] initWithFrame:CGRectMake(10, 70, self.view.frame.size.width - 70, 70)];
-    self.titleField.text = @"Enter Title Here";
+    self.titleField.text = @"";
     self.titleField.clearButtonMode = UITextFieldViewModeAlways;
     self.titleField.delegate = self;
     [self.view addSubview:self.titleField];
     
     self.textNote = [[UITextView alloc] initWithFrame:CGRectMake(10, 145, self.view.frame.size.width - 10, self.view.frame.size.height - 145)];
-    self.textNote.text = @"Place notes and other related text in this area.";
+    self.textNote.text = @"";
     self.textNote.delegate = self;
     [self.view addSubview:self.textNote];
     
--- DayX/DXListViewController.m
+++ DayX/DXListViewController.m
@@ -23,7 +23,7 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-        [self.tableView reloadData];
+    [self.tableView reloadData];
 }
 
 - (void)viewDidLoad
