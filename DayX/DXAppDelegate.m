1f6221861a3e6c000f099e29693ed4962b53142b
James Westmoreland <jr@jrw.org>
2014-11-17 09:53:09 -0700

    Removed comment line that was not required.

--- DayX/DXAppDelegate.m
+++ DayX/DXAppDelegate.m
@@ -16,7 +16,6 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    // Override point for customization after application launch.
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[DXListViewController new]];
     
     self.window.backgroundColor = [UIColor whiteColor];
